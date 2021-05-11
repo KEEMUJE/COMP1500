b7522e620486aa20c795f8be871a10dc57cb56e4
김우재 <1999.mozzi@gmail.com>
2021-05-11 11:38:52 +0900

    Assignment1 제출

--- Assignment1/Assignment1/Assignment1.cs
+++ Assignment1/Assignment1/Assignment1.cs
@@ -7,45 +7,57 @@ namespace Assignment1
         public static void PrintIntegers(StreamReader input, StreamWriter output, int width)
         {
             int[] integerArrays = new int[5];
+            width = 10;
 
             for (int arrayNumbers = 0; arrayNumbers < 5; arrayNumbers++)
             {
                 integerArrays[arrayNumbers] = int.Parse(input.ReadLine());
             }
 
-            output.WriteLine("{0,14}{1,14}{2,14}", "oct", "dec", "hex");
+            output.WriteLine("{0,10}{1,10}{2,10}", "oct", "dec", "hex");
 
             for (int outputNumbers = 0; outputNumbers < 5; outputNumbers++)
             {
                 // COMP1500 문자열 강의, 레퍼런스 String.Format 메서드 참고하기.
-                output.Write("{0, 14}", System.Convert.ToString(integerArrays[outputNumbers], 8));
-                output.Write("{0, 14}", System.Convert.ToString(integerArrays[outputNumbers], 10));
-                output.Write("{0, 14}", System.Convert.ToString(integerArrays[outputNumbers], 16) + "\n");
+                output.Write("{0, 10}", System.Convert.ToString(integerArrays[outputNumbers], 8));
+                output.Write("{0, 10}", System.Convert.ToString(integerArrays[outputNumbers], 10));
+                output.Write("{0, 10}", System.Convert.ToString(integerArrays[outputNumbers], 16));
+                output.WriteLine("");
             }
         }
 
         public static void PrintStats(StreamReader input, StreamWriter output)
         {
-            float[] floatArrays = new float[5];
+            double[] doubleArrays = new double[5];
 
             for (int arrayNumbers = 0; arrayNumbers < 5; arrayNumbers++)
             {
-                floatArrays[arrayNumbers] = float.Parse(input.ReadLine());
+                doubleArrays[arrayNumbers] = double.Parse(input.ReadLine());
                
                 if (arrayNumbers == 4)
                 {
                     for (arrayNumbers = 0; arrayNumbers < 5; arrayNumbers++)
                     {
-                        double truncateFloats = System.Math.Truncate(floatArrays[arrayNumbers] * 10000) / 10000;
-                        output.WriteLine("{0,25}", floatArrays[arrayNumbers].ToString("f3"));
+                        output.WriteLine("{0,25}", doubleArrays[arrayNumbers].ToString("f3"));
                     }
                     
-                    System.Array.Sort(floatArrays);
-                    double floatArraysSum = floatArrays[0] + floatArrays[1] + floatArrays[2] + floatArrays[3] + floatArrays[4];
-                    output.WriteLine("{0,-11}{1,14}", "Min", floatArrays[0].ToString("f3"));
-                    output.WriteLine("{0,-11}{1,14}", "Max", floatArrays[4].ToString("f3"));
-                    output.WriteLine("{0,-11}{1,14}", "Sum", floatArraysSum.ToString("f3"));
-                    output.WriteLine("{0,-11}{1,14}", "Average", (floatArraysSum / 5).ToString("f3"));
+                    System.Array.Sort(doubleArrays);
+                    double doubleArraysSum = doubleArrays[0] + doubleArrays[1] + doubleArrays[2] + doubleArrays[3] + doubleArrays[4];
+                    if (doubleArraysSum > 99999999999999)
+                    {
+                        output.WriteLine("{0,-7}{1,18}", "Min", doubleArrays[0].ToString("f3"));
+                        output.WriteLine("{0,-7}{1,18}", "Max", doubleArrays[4].ToString("f3"));
+                        output.WriteLine("{0,-6}{1,19}", "Sum", doubleArraysSum.ToString("f3"));
+                        output.WriteLine("{0,-7}{1,18}", "Average", (doubleArraysSum / 5).ToString("f3"));
+                    }
+
+                    else
+                    {
+                        output.WriteLine("{0,-7}{1,18}", "Min", doubleArrays[0].ToString("f3"));
+                        output.WriteLine("{0,-7}{1,18}", "Max", doubleArrays[4].ToString("f3"));
+                        output.WriteLine("{0,-7}{1,18}", "Sum", doubleArraysSum.ToString("f3"));
+                        output.WriteLine("{0,-7}{1,18}", "Average", (doubleArraysSum / 5).ToString("f3"));
+                    }
                 }
             }
         }
