573b90810101dd85d1bc8e9759490a91afaa6327
김우재 <1999.mozzi@gmail.com>
2021-05-11 20:00:00 +0900

    Assignment1 Commit

--- Assignment1/Assignment1/Program.cs
+++ Assignment1/Assignment1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Text;
 
 namespace Assignment1
 {
@@ -7,15 +8,20 @@ namespace Assignment1
     {
         static void Main(string[] args)
         {
-            // using 문에 대해서는 아직 고민하지 마세요. 나중에 배웁니다.
-            using (StreamReader READER = new StreamReader(Console.OpenStandardInput()))
+            string integerInput = "52\n102\n299999999\n2382\n409"; // PrintIntegers() Auto input
+            string floatInput = "2.23\n-1912.87233125\n2323\n1\n34.55567"; // PrintStats() Auto input
+            byte[] buffer1 = Encoding.UTF8.GetBytes(integerInput);
+            byte[] buffer2 = Encoding.UTF8.GetBytes(floatInput);
+
+            using (StreamReader READER1 = new StreamReader(new MemoryStream(buffer1)))
+            using (StreamReader READER2 = new StreamReader(new MemoryStream(buffer2)))
             using (StreamWriter WRITER = new StreamWriter(Console.OpenStandardOutput()) { AutoFlush = true })
             {
-                Console.WriteLine("Enter your 5 digits for PrintIntegers():");
-                Assignment1.PrintIntegers(READER, WRITER, 14);
+                Console.WriteLine("PrintIntegers()\n");
+                Assignment1.PrintIntegers(READER1, WRITER, 14); // width : 14
 
-                Console.WriteLine("Enter your 5 digits for PrintStats():");
-                Assignment1.PrintStats(READER, WRITER);
+                Console.WriteLine("\nPrintStats()\n");
+                Assignment1.PrintStats(READER2, WRITER);
             }
         }
     }
