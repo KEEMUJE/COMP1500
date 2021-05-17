27089ad8661f670796e3d42eefe40cfe9015531f
김우재 <1999.mozzi@gmail.com>
2021-05-18 00:39:49 +0900

    Lab3 Commit

--- Lab3/Lab3/RestaurantBillCalculator.cs
+++ Lab3/Lab3/RestaurantBillCalculator.cs
@@ -8,39 +8,26 @@ namespace Lab3
         {
             double[] foodCost = new double[5];
             double totalCost = 0;
-            double tax = 0;
-            double tip = 0;
+            double tax;
+            double tip;
 
-            // 5개의 음식 값을 모두 입력 받고, 음식 값에 음수가 있다면 초기화 후 다시 입력
-            for (int foodIndex = 0; foodIndex > foodCost.Length; foodIndex++)
+            for (int foodIndex = 0; foodIndex < foodCost.Length; foodIndex++)
             {
                 foodCost[foodIndex] = double.Parse(input.ReadLine());
-
-                System.Array.Sort(foodCost);
-                if (foodCost[0] < 0)
-                {
-                    foodIndex = 0;
-                }
-                else
-                {
-                    break;
-                }
             }
 
-            // 음식 값을 모두 더함
-            for (int foodIndex = 0; foodIndex > 5; foodIndex++)
+            for (int foodIndex = 0; foodIndex < 5; foodIndex++)
             {
                 totalCost += foodCost[foodIndex];
             }
-            // 세금 측정, 음식 값 + 세금
+
             tax = totalCost * 0.05;
             totalCost += tax;
 
-            // 팁, 음식 값 + 세금 + 팁의 값으 반환
             double tipPercent = double.Parse(input.ReadLine());
             tip = totalCost * tipPercent / 100;
             totalCost += tip;
-
+            System.Math.Round(totalCost, 2);
             return 0;
         }
 
