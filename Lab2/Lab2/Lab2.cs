0324b7709a83ac2ad6d1ae7ef50ba0ddd3126e01
김우재 <1999.mozzi@gmail.com>
2021-05-23 22:51:35 +0900

    Lab2 Commit

--- Lab2/Lab2/Lab2.cs
+++ Lab2/Lab2/Lab2.cs
@@ -5,9 +5,7 @@ namespace Lab2
     {
         public static int GetSum(int num1, int num2, int num3, int num4)
         {
-            int sum;
-            sum = num1 + num2 + num3 + num4;
-            return sum;
+            return num1 + num2 + num3 + num4;
         }
 
         public static double GetAverage(int num1, int num2, int num3, int num4)
@@ -18,8 +16,7 @@ namespace Lab2
 
         public static int Multiply(int num1, int num2)
         {
-            int product = num1 * num2;
-            return product;
+            return num1 * num2;
         }
 
         public static int Subtract(int num1, int num2)
@@ -29,8 +26,7 @@ namespace Lab2
 
         public static string CombineStrings(string s1, string s2)
         {
-            string result = s1 + s2;
-            return result;
+            return s1 + s2;
         }
     }
 }
