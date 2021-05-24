f250df6ce209c23e4ad0329a3ca637c94930e577
김우재 <1999.mozzi@gmail.com>
2021-05-24 11:58:17 +0900

    Lab4 Commit

---
+++ Lab4/Lab4/Calendar.cs
@@ -0,0 +1,42 @@
+﻿namespace Lab4
+{
+    public static class Calendar
+    {
+        public static bool IsLeapYear(uint year)
+        {
+            bool bLeapYear;
+
+            if (year % 100 == 0)
+            {
+                bLeapYear = false;
+            }
+            else if (year % 400 == 0)
+            {
+                bLeapYear = false;
+            }
+            else if (year % 4 == 0)
+            {
+                bLeapYear = true;
+            }
+            else
+            {
+                bLeapYear = false;
+            }
+            return bLeapYear;
+        }
+
+        public static int GetDaysInMonth(uint year, uint month)
+        {
+            if (year > 9999)
+            {
+                return -1;
+            }
+            else if (month < 0 || month > 12)
+            {
+                return -1;
+            }
+
+            return -1;
+        }
+    }
+}
