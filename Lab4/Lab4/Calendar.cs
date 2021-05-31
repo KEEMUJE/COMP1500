2e7ce8446e43dbe61fa6c05b81fa2b20820894d3
김우재 <1999.mozzi@gmail.com>
2021-05-31 09:45:32 +0900

    Lab4 Commit (Refactoring)

--- Lab4/Lab4/Calendar.cs
+++ Lab4/Lab4/Calendar.cs
@@ -4,7 +4,7 @@
     {
         public static bool IsLeapYear(uint year)
         {
-            return (year % 4 == 0 && year % 100 == 0 && year % 400 == 0) || (year % 4 == 0 && year % 100 != 0);
+            return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
         }
 
         public static int GetDaysInMonth(uint year, uint month)
