9a80c5eec35d535e122dce35ff2a64f650455386
김우재 <1999.mozzi@gmail.com>
2021-05-31 23:37:03 +0900

    Lab4 Commit (Refactoring)

--- Lab4/Lab4/Calendar.cs
+++ Lab4/Lab4/Calendar.cs
@@ -22,14 +22,12 @@
             {
                 return 30;
             }
-            else if (month == 2 && (year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+            else if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
             {
                 return 29;
             }
-            else
-            {
+
                 return 28;
-            }
 
             /*
             // Solution 2
