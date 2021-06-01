f3122a083d01dbca02073cadc9f9d06710b9c49d
김우재 <1999.mozzi@gmail.com>
2021-06-01 09:23:09 +0900

    Lab5 Commit

--- Lab5/Lab5/Lab5.cs
+++ Lab5/Lab5/Lab5.cs
@@ -8,24 +8,44 @@ namespace Lab5
         {
             int length = revenuePerDay.Length;
             double[] fixRevenue = new double[length];
+            bool[] bArray = new bool[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                fixRevenue[i] = revenuePerDay[i];
+            }
 
             for (int i = 0; i < length; i++)
             {
                 if (usersPerDay[i] <= 10)
                 {
-                    revenuePerDay[i] = usersPerDay[i] / 2.0;
+                    revenuePerDay[i] = Math.Round(usersPerDay[i] / 2.0, 2);
                 }
                 else if (usersPerDay[i] <= 100)
                 {
-                    revenuePerDay[i] = (16 * usersPerDay[i] / 5.0) - 27;
+                    revenuePerDay[i] = Math.Round((16 * usersPerDay[i] / 5.0) - 27, 2);
                 }
                 else if (usersPerDay[i] <= 1000)
                 {
-                    revenuePerDay[i] = (usersPerDay[i] * usersPerDay[i] / 4.0) - (2 * usersPerDay[i]) - 2007;
+                    revenuePerDay[i] = Math.Round((usersPerDay[i] * usersPerDay[i] / 4.0) - (2 * usersPerDay[i]) - 2007, 2);
                 }
                 else
                 {
-                    revenuePerDay[i] = 245743 + (usersPerDay[i] / 4.0);
+                    revenuePerDay[i] = Math.Round(245743 + (usersPerDay[i] / 4.0), 2);
+                }
+
+
+                if (fixRevenue[i] != revenuePerDay[i])
+                {
+                    bArray[i] = true;
+                }
+            }
+
+            for (int i = 0; i < length; i++)
+            {
+                if (bArray[i] == true)
+                {
+                    return true;
                 }
             }
 
