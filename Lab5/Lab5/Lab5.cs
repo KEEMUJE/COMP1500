fe0d708c3c063780614a5d6fc8d5f91ddf16537b
김우재 <1999.mozzi@gmail.com>
2021-06-07 12:09:04 +0900

    Lab5 Commit (Refactoring)

--- Lab5/Lab5/Lab5.cs
+++ Lab5/Lab5/Lab5.cs
@@ -6,103 +6,79 @@ namespace Lab5
     {
         public static bool TryFixData(uint[] usersPerDay, double[] revenuePerDay)
         {
-            int uLength = usersPerDay.Length;
-            int rLength = revenuePerDay.Length;
-            double[] fixRevenue = new double[rLength];
-            bool[] bArray = new bool[rLength];
+            double fixRevenue;
+            bool bIsFixed = false;
 
-            if (uLength != rLength)
+            if (usersPerDay.Length != revenuePerDay.Length)
             {
                 return false;
             }
 
-            for (int i = 0; i < rLength; i++)
-            {
-                fixRevenue[i] = revenuePerDay[i];
-            }
-
-            for (int i = 0; i < rLength; i++)
+            for (int i = 0; i < usersPerDay.Length; i++)
             {
                 if (usersPerDay[i] <= 10)
                 {
-                    revenuePerDay[i] = Math.Round(usersPerDay[i] / 2.0, 2);
+                    fixRevenue = Math.Round(usersPerDay[i] / 2.0, 2);
                 }
 
                 else if (usersPerDay[i] <= 100)
                 {
-                    revenuePerDay[i] = Math.Round((16 * usersPerDay[i] / 5.0) - 27, 2);
+                    fixRevenue = Math.Round(16 * usersPerDay[i] / 5.0 - 27, 2);
                 }
 
                 else if (usersPerDay[i] <= 1000)
                 {
-                    revenuePerDay[i] = Math.Round((usersPerDay[i] * usersPerDay[i] / 4.0) - (2 * usersPerDay[i]) - 2007, 2);
+                    fixRevenue = Math.Round(usersPerDay[i] * usersPerDay[i] / 4.0 - 2 * usersPerDay[i] - 2007, 2);
                 }
 
                 else
                 {
-                    revenuePerDay[i] = Math.Round(245743 + (usersPerDay[i] / 4.0), 2);
+                    fixRevenue = Math.Round(245743 + usersPerDay[i] / 4.0, 2);
                 }
 
-                if (fixRevenue[i] != revenuePerDay[i])
+                if (revenuePerDay[i] != fixRevenue)
                 {
-                    bArray[i] = true;
+                    bIsFixed = true;
+                    revenuePerDay[i] = fixRevenue;
                 }
             }
 
-            for (int i = 0; i < rLength; i++)
-            {
-                if (bArray[i] == true)
-                {
-                    return true;
-                }
-            }
-
-            return false;
+            return bIsFixed;
         }
 
         public static int GetInvalidEntryCount(uint[] usersPerDay, double[] revenuePerDay)
         {
             int wrongElement = 0;
-            int uLength = usersPerDay.Length;
-            int rLength = revenuePerDay.Length;
-            double[] fixRevenue = new double[rLength];
+            double fixRevenue;
 
-            if (uLength != rLength)
+            if (usersPerDay.Length != revenuePerDay.Length)
             {
                 return -1;
             }
 
-            for (int i = 0; i < rLength; i++)
-            {
-                fixRevenue[i] = revenuePerDay[i];
-            }
-
-            for (int i = 0; i < rLength; i++)
+            for (int i = 0; i < usersPerDay.Length; i++)
             {
                 if (usersPerDay[i] <= 10)
                 {
-                    fixRevenue[i] = Math.Round(usersPerDay[i] / 2.0, 2);
+                    fixRevenue = Math.Round(usersPerDay[i] / 2.0, 2);
                 }
 
                 else if (usersPerDay[i] <= 100)
                 {
-                    fixRevenue[i] = Math.Round((16 * usersPerDay[i] / 5.0) - 27, 2);
+                    fixRevenue = Math.Round(16 * usersPerDay[i] / 5.0 - 27, 2);
                 }
 
                 else if (usersPerDay[i] <= 1000)
                 {
-                    fixRevenue[i] = Math.Round((usersPerDay[i] * usersPerDay[i] / 4.0) - (2 * usersPerDay[i]) - 2007, 2);
+                    fixRevenue = Math.Round(usersPerDay[i] * usersPerDay[i] / 4.0 - 2 * usersPerDay[i] - 2007, 2);
                 }
 
                 else
                 {
-                    fixRevenue[i] = Math.Round(245743 + (usersPerDay[i] / 4.0), 2);
+                    fixRevenue = Math.Round(245743 + usersPerDay[i] / 4.0, 2);
                 }
-            }
 
-            for (int i = 0; i < rLength; i++)
-            {
-                if (fixRevenue[i] != revenuePerDay[i])
+                if (fixRevenue != revenuePerDay[i])
                 {
                     wrongElement += 1;
                 }
@@ -113,10 +89,9 @@ namespace Lab5
 
         public static double CalculateTotalRevenue(double[] revenuePerDay, uint start, uint end)
         {
-            int rLength = revenuePerDay.Length - 1;
             double totalRevenue = 0;
 
-            if (start < 0 || end > rLength || revenuePerDay.Length == 0 || start > end)
+            if (revenuePerDay.Length == 0 || start < 0 || end >= revenuePerDay.Length || start > end)
             {
                 return -1;
             }
