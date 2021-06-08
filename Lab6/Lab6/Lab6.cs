ddd7683ccd5ed1a86aabc3b12d72b966806fa396
김우재 <1999.mozzi@gmail.com>
2021-06-08 12:47:41 +0900

    Lab5 Commit

--- Lab6/Lab6/Lab6.cs
+++ Lab6/Lab6/Lab6.cs
@@ -9,13 +9,13 @@ namespace Lab6
             int rotatedRow = 0;
             int rotatedColumn = 0;
 
-            for (int i = data.GetLength(1) - 1; i >= 0; i--)
+            for (int i = 0; i < data.GetLength(1); i++)
             {
                 rotatedColumn = 0;
 
-                for (int j = 0; j < data.GetLength(0); j++)
+                for (int j = data.GetLength(0) - 1; j >= 0; j--)
                 {
-                    rotated[rotatedRow, rotatedColumn] = data[i, j];
+                    rotated[rotatedRow, rotatedColumn] = data[j, i];
                     rotatedColumn++;
                 }
 
@@ -27,6 +27,16 @@ namespace Lab6
 
         public static int[,] TransformArray(int[,] data, EMode mode)
         {
+            int[,] copyData = new int[data.GetLength(0), data.GetLength(1)];
+
+            for (int i = 0; i < data.GetLength(0); i++)
+            {
+                for (int j = 0; j < data.GetLength(1); j++)
+                {
+                    copyData[i, j] = data[i, j];
+                }
+            }
+
             switch (mode)
             {
                 case EMode.HorizontalMirror:
@@ -34,15 +44,35 @@ namespace Lab6
                     {
                         for (int j = 0; j < data.GetLength(1); j++)
                         {
-                            data[i, j] = data[i, data.GetLength(1) - 1 - j];
+                            data[i, j] = copyData[i, data.GetLength(1) - 1 - j];
                         }
                     }
                     break;
 
                 case EMode.VerticalMirror:
+                    for (int i = 0; i < data.GetLength(0); i++)
+                    {
+                        for (int j = 0; j < data.GetLength(1); j++)
+                        {
+                            data[i, j] = copyData[data.GetLength(0) - 1 - i, j];
+                        }
+                    }
                     break;
 
                 case EMode.DiagonalShift:
+                    int dataRow = data.GetLength(0) - 1;
+                    int dataColumn = data.GetLength(1) - 1;
+
+                    for (int i = 0; i < data.GetLength(0); i++)
+                    {
+                        for (int j = 0; j < data.GetLength(1); j++)
+                        {
+                            data[i, j] = copyData[dataRow, dataColumn];
+                            dataColumn = j;
+                        }
+
+                        dataRow = i;
+                    }
                     break;
 
                 default:
