0bc6288dd0959d44d889b0d72584a7f5f158b4bf
김우재 <1999.mozzi@gmail.com>
2021-06-18 16:42:27 +0900

    Calendar.cs GetDaysInMonth function refactoring (switch statements)

--- Lab4/Lab4/Calendar.cs
+++ Lab4/Lab4/Calendar.cs
@@ -10,6 +10,38 @@
         public static int GetDaysInMonth(uint year, uint month)
         {
             // Solution 1
+            switch (month)
+            {
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                case 8:
+                case 10:
+                case 12:
+                    return 31;
+
+                case 4:
+                case 6:
+                case 9:
+                case 11:
+                    return 30;
+
+                case 2:
+                    if (IsLeapYear(year))
+                    {
+                        return 29;
+                    }
+
+                    return 28;
+
+                default:
+                    break;
+            }
+
+            return -1;
+
+            /* Solution 2
             if (year > 9999 || month < 1 || month > 12)
             {
                 return -1;
@@ -22,31 +54,12 @@
             {
                 return 30;
             }
-            else if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+            else if (IsLeapYear(year))
             {
                 return 29;
             }
 
-                return 28;
-
-            /*
-            // Solution 2
-            if (year > 9999 || month < 1 || month > 12)
-            {
-                return -1;
-            }
-            else if (month == 2)
-            {
-                return IsLeapYear(year) ? 29 : 28;
-            }
-            else if (month <= 7)
-            {
-                return month % 2 == 0 ? 30 : 31;
-            }
-            else
-            {
-                return month % 2 == 0 ? 31 : 30;
-            }
+            return 28;
             */
         }
     }
