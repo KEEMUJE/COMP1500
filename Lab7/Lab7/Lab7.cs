13ad80f9cf6796f57df00ea8a59d7d21277a34b8
Jaech <1999.mozzi@gmail.com>
2021-06-28 23:44:37 +0900

    Lab7 commit

---
+++ Lab7/Lab7/Lab7.cs
@@ -0,0 +1,23 @@
+﻿namespace Lab7
+{
+    public static class Lab7
+    {
+        public static bool PlayGame(uint[] array)
+        {
+            if (array.Length <= 1 || array[0] > array.Length - 1)
+            {
+                return false;
+            }
+
+            return false;
+        }
+
+        public static bool GameVerification(uint[] array)
+        {
+            // 종료 조건 = array[array.Length - 1]에 도착할 때 || 도착하지 못한다고 판별 됐을 때.
+            // array = new uint[9] { 4, 3, 4, 4, 1, 5, 7, 1, 0 };
+
+            return true;
+        }
+    }
+}
