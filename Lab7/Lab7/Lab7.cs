1f99e7f2cb464bbd82d446f1f009e6a2a362722b
김우재 <1999.mozzi@gmail.com>
2021-06-30 23:44:28 +0900

    test build

--- Lab7/Lab7/Lab7.cs
+++ Lab7/Lab7/Lab7.cs
@@ -9,40 +9,7 @@
                 return false;
             }
 
-            if (array[array[0]] == array[array.Length - 1] || (array[array.Length - 2] == 1 && array[0] == array.Length - 2))
-            {
-                return true;
-            }
-
-            uint[] checkArray = new uint[array.Length - 1];
-
-            for (int i = 0; i < checkArray.Length; i++)
-            {
-                checkArray[i] = array[i + 1];
-            }
-
-            if (checkArray[0] == checkArray.Length - 1)
-            {
-                return true;
-            }
-
-            return PlayGame(checkArray);
-
-
-
-            /*
-            uint[] checkArray = new uint[array.Length - 1];
-
-            checkArray[0] = array[array.Length - 1];
-
-            if (checkArray[0] == checkArray.Length - 1)
-            {
-                return true;
-            }
-
-            /*
             uint[] checkArray;
-
             if (array[0] == array[array.Length - 2])
             {
                 checkArray = new uint[array.Length - 1];
@@ -58,9 +25,8 @@
             {
                 return true;
             }
-            */
 
-            // return PlayGame(checkArray);
+            return PlayGame(checkArray);
         }
     }
 }
