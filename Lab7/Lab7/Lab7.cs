4f4d877ca854be675f85ce01c88eafa3903b6c71
김우재 <1999.mozzi@gmail.com>
2021-07-01 02:02:29 +0900

    test build

--- Lab7/Lab7/Lab7.cs
+++ Lab7/Lab7/Lab7.cs
@@ -4,19 +4,9 @@
     {
         public static bool PlayGame(uint[] array)
         {
-            // array = new uint[9] { 4, 3, 4, 4, 1, 5, 7, 1, 0 };
-            // bPass = Lab7.PlayGame(array); // true
-            // array = new uint[10] { 6, 1, 5, 2, 4, 8, 2, 1, 4, 0 };
-            // bPass = Lab7.PlayGame(array); // false
-
             if (array.Length <= 1 || array[0] > array.Length - 1 || array[0] == 0)
             {
                 return false;
-            }
-
-            if (array[0] == array.Length - 1)
-            {
-                return true;
             }
 
             uint[] checkArray;
@@ -30,7 +20,7 @@
                 checkArray = new uint[array.Length - array[0]];
             }
 
-            checkArray[0] = array[array.Length - 2];
+            checkArray[0] = array[array.Length - 1];
 
             if (checkArray[0] == checkArray.Length - 1)
             {
