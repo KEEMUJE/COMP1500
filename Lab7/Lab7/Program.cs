bc083f0ff13bd9aeb03fd0cdca450f954e6f2439
김우재 <1999.mozzi@gmail.com>
2021-06-30 14:38:52 +0900

    Modify test code

--- Lab7/Lab7/Program.cs
+++ Lab7/Lab7/Program.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 
 namespace Lab7
 {
@@ -11,26 +10,6 @@ namespace Lab7
             bool bPass = Lab7.PlayGame(array);
 
             Debug.Assert(!bPass);
-
-            array = new uint[3] { 3, 2, 0 };
-            bPass = Lab7.PlayGame(array); // false
-
-            Debug.Assert(!bPass);
-
-            array = new uint[3] { 2, 2, 0 };
-            bPass = Lab7.PlayGame(array); // true
-
-            Debug.Assert(bPass);
-
-            array = new uint[3] { 1, 1, 0 };
-            bPass = Lab7.PlayGame(array); // true
-
-            Debug.Assert(bPass);
-
-            array = new uint[7] { 3, 1, 4, 2, 8, 6, 0 };
-            bPass = Lab7.PlayGame(array);
-
-            Debug.Assert(bPass);
 
             array = new uint[1] { 0 };
             bPass = Lab7.PlayGame(array);
@@ -52,48 +31,18 @@ namespace Lab7
 
             Debug.Assert(bPass);
 
-            array = new uint[8] { 2, 2, 2, 2, 1, 2, 2, 0 };
-            bPass = Lab7.PlayGame(array);
-
-            Debug.Assert(bPass);
-
             array = new uint[10] { 6, 1, 5, 2, 4, 8, 2, 1, 4, 0 };
             bPass = Lab7.PlayGame(array);
 
             Debug.Assert(!bPass);
-
-            array = new uint[12] { 5, 5, 11, 3, 7, 3, 2, 1, 1, 4, 3, 0 };
-            bPass = Lab7.PlayGame(array);
-
-            Debug.Assert(bPass);
 
             array = new uint[12] { 3, 6, 2, 4, 4, 6, 1, 1, 4, 5, 1, 0 };
             bPass = Lab7.PlayGame(array);
 
             Debug.Assert(bPass);
 
-            array = new uint[5] { 2, 1, 1, 1, 0 };
+            array = new uint[12] { 5, 5, 11, 3, 7, 3, 2, 1, 1, 4, 3, 0 }; // B07_ValidStartIndexTrueHelpful
             bPass = Lab7.PlayGame(array);
-
-            Debug.Assert(bPass);
-
-            array = new uint[] { 4, 2, 3, 4, 6, 3, 5, 2, 0 };
-            bPass = Lab7.PlayGame(array);
-
-            Debug.Assert(!bPass);
-
-            array = new uint[] { 5, 4, 1, 2, 1, 2, 0 };
-            bPass = Lab7.PlayGame(array);
-
-            Debug.Assert(!bPass);
-
-            array = new uint[] { 7, 4, 1, 2, 1, 2, 1, 2, 0 };
-            bPass = Lab7.PlayGame(array);
-
-            Debug.Assert(!bPass);
-
-            array = new uint[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0 };
-            bPass = Lab7.PlayGame(array); // true
 
             Debug.Assert(bPass);
         }
