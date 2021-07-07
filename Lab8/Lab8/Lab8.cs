a9d5e9b59283e0353b9ac4b830e0892659049e0c
김우재 <1999.mozzi@gmail.com>
2021-07-07 12:34:54 +0900

    Using recursive stringbuilder

---
+++ Lab8/Lab8/Lab8.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text;
+
+namespace Lab8
+{
+    public static class Lab8
+    {
+        public static string PrettifyList(string s)
+        {
+            if (s.Length == 0)
+            {
+                return null;
+            }
+
+            const int CAPACITY = 4096;
+            string[] verticalSplit = s.Split('|');
+
+            StringBuilder tempBuilder = new StringBuilder(CAPACITY);
+
+            PrettifyListRecursive(verticalSplit, tempBuilder, 0);
+
+            return tempBuilder.ToString();
+        }
+
+        public static string PrettifyListRecursive(string[] verticalSplit, StringBuilder tempBuilder, int index)
+        {
+            if (index == verticalSplit.Length)
+            {
+                return tempBuilder.ToString();
+            }
+
+            int asciiValue = 97;
+
+            tempBuilder.AppendLine($"{index + 1}) {verticalSplit[index]}");
+
+            int underIndex = tempBuilder.ToString().IndexOf('_');
+
+            while (underIndex != -1)
+            {
+                tempBuilder.Replace("_", $"\n    {Convert.ToChar(asciiValue++)}) ", underIndex, 1);
+                underIndex = tempBuilder.ToString().IndexOf('_');
+            }
+
+            tempBuilder.Replace("/", "\n        - ");
+
+            PrettifyListRecursive(verticalSplit, tempBuilder, index + 1);
+
+            return null;
+        }
+    }
+}
