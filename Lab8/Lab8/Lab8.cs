2f99cc291ed26b0285ea583803f2e27d2dec0bee
김우재 <1999.mozzi@gmail.com>
2021-07-07 21:28:52 +0900

    Improve code structure

--- Lab8/Lab8/Lab8.cs
+++ Lab8/Lab8/Lab8.cs
@@ -15,9 +15,7 @@ namespace Lab8
 
             const int CAPACITY = 4096;
             string[] verticalSplit = s.Split('|');
-
             StringBuilder tempBuilder = new StringBuilder(CAPACITY);
-
             List<String> convert = PrettifyListRecursive(new List<string>(), tempBuilder, verticalSplit, 0);
 
             return string.Join("", convert);
@@ -31,16 +29,29 @@ namespace Lab8
             }
 
             int asciiValue = 97;
+            int asciiLoop = 0;
 
-            tempBuilder.AppendLine($"{index + 1}) {verticalSplit[index]}");
+            verticalSplit[index] = verticalSplit[index].Insert(0, $"{index + 1}) ");
+            var underSplit = verticalSplit[index].Split('_');
+            tempBuilder.AppendLine(underSplit[0].ToString());
 
-            int underIndex = tempBuilder.ToString().IndexOf('_');
-
-            while (underIndex != -1)
+            for (int i = 1; i < underSplit.Length; i++)
             {
-                tempBuilder.Replace("_", $"\n    {Convert.ToChar(asciiValue++)}) ", underIndex, 1);
-                tempBuilder.Replace("/", "\n        - ");
-                underIndex = tempBuilder.ToString().IndexOf('_');
+                underSplit[i] = underSplit[i].Insert(0, $"    {Convert.ToChar(asciiValue++)}) ");
+
+                for (int j = 0; j < asciiLoop; j++)
+                {
+                    underSplit[i] = underSplit[i].Insert(underSplit[i].IndexOf($"{Convert.ToChar(asciiValue - 1)}"), $"{Convert.ToChar(asciiValue - 1)}");
+                }
+
+                underSplit[i] = underSplit[i].Replace("/", "\n        - ");
+                tempBuilder.AppendLine(underSplit[i].ToString());
+
+                if (asciiValue > 122)
+                {
+                    asciiValue = 97;
+                    asciiLoop += 1;
+                }
             }
 
             outConsole.Add(tempBuilder.ToString());
