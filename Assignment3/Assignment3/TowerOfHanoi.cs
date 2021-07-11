9bccbb12848dab08b884fced8da1d963ce323d8e
김우재 <1999.mozzi@gmail.com>
2021-07-11 14:55:45 +0900

    GetNumberOfSteps build

---
+++ Assignment3/Assignment3/TowerOfHanoi.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Assignment3
+{
+    public static class TowerOfHanoi
+    {
+
+        public static int GetNumberOfSteps(int numDiscs)
+        {
+            if (numDiscs < 0)
+            {
+                return -1;
+            }
+
+            return (int)Math.Pow(2, numDiscs) - 1;
+        }
+
+        public static List<List<int>[]> SolveTowerOfHanoi(int numDiscs)
+        {
+            if (numDiscs < 1)
+            {
+                return null;
+            }
+
+            return null;
+        }
+    }
+}
