0d7e5b7fbf730fdfefd6eb8b65a9ed677e4776e2
김우재 <1999.mozzi@gmail.com>
2021-07-11 12:29:27 +0900

    Rename Recursive Functions and Variables

--- Lab8/Lab8/Lab8.cs
+++ Lab8/Lab8/Lab8.cs
@@ -15,13 +15,12 @@ namespace Lab8
 
             const int CAPACITY = 4096;
             string[] verticalSplit = s.Split('|');
-            StringBuilder tempBuilder = new StringBuilder(CAPACITY);
-            List<String> convert = PrettifyListRecursive(new List<string>(), tempBuilder, verticalSplit, 0);
+            List<String> prettifyList = ConvertPrettifyListRecursive(new List<string>(), new StringBuilder(CAPACITY), verticalSplit, 0);
 
-            return string.Join("", convert);
+            return string.Join("", prettifyList);
         }
 
-        public static List<string> PrettifyListRecursive(List<string> outConsole, StringBuilder tempBuilder, string[] verticalSplit, int index)
+        public static List<string> ConvertPrettifyListRecursive(List<string> outConsole, StringBuilder prettifyBuilder, string[] verticalSplit, int index)
         {
             if (index == verticalSplit.Length)
             {
@@ -33,10 +32,16 @@ namespace Lab8
 
             verticalSplit[index] = verticalSplit[index].Insert(0, $"{index + 1}) ");
             var underSplit = verticalSplit[index].Split('_');
-            tempBuilder.AppendLine(underSplit[0].ToString());
+            prettifyBuilder.AppendLine(underSplit[0].ToString());
 
             for (int i = 1; i < underSplit.Length; i++)
             {
+                if (asciiValue > 122)
+                {
+                    asciiValue = 97;
+                    asciiLoop += 1;
+                }
+
                 underSplit[i] = underSplit[i].Insert(0, $"    {Convert.ToChar(asciiValue++)}) ");
 
                 for (int j = 0; j < asciiLoop; j++)
@@ -45,19 +50,13 @@ namespace Lab8
                 }
 
                 underSplit[i] = underSplit[i].Replace("/", "\n        - ");
-                tempBuilder.AppendLine(underSplit[i].ToString());
-
-                if (asciiValue > 122)
-                {
-                    asciiValue = 97;
-                    asciiLoop += 1;
-                }
+                prettifyBuilder.AppendLine(underSplit[i].ToString());
             }
 
-            outConsole.Add(tempBuilder.ToString());
-            tempBuilder.Clear();
+            outConsole.Add(prettifyBuilder.ToString());
+            prettifyBuilder.Clear();
 
-            PrettifyListRecursive(outConsole, tempBuilder, verticalSplit, index + 1);
+            ConvertPrettifyListRecursive(outConsole, prettifyBuilder, verticalSplit, index + 1);
 
             return outConsole;
         }
