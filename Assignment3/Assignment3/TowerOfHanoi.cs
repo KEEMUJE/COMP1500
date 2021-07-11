41defafd5a99d54baf073610e9dda9303aeba3aa
김우재 <1999.mozzi@gmail.com>
2021-07-11 22:25:28 +0900

    Assignment TowerOfHanoi Function Build!

--- Assignment3/Assignment3/TowerOfHanoi.cs
+++ Assignment3/Assignment3/TowerOfHanoi.cs
@@ -5,6 +5,7 @@ namespace Assignment3
 {
     public static class TowerOfHanoi
     {
+        const byte NUMBER_OF_STICKS = 3;
 
         public static int GetNumberOfSteps(int numDiscs)
         {
@@ -23,7 +24,45 @@ namespace Assignment3
                 return null;
             }
 
-            return null;
+            int index = 0;
+
+            List<List<int>[]> snapshots = new List<List<int>[]>();
+            snapshots.Add(new List<int>[NUMBER_OF_STICKS]);
+
+            for (int i = 0; i < NUMBER_OF_STICKS; i++)
+            {
+                snapshots[index][i] = new List<int>();
+            }
+
+            var from = snapshots[index][0];
+            var aux = snapshots[index][1];
+            var to = snapshots[index][2];
+
+            for (int i = 0; i < numDiscs; i++)
+            {
+                from.Add(numDiscs - i);
+            }
+
+            return SolveTowerOfHanoiRecursive(numDiscs, ++index, snapshots, from, aux, to);
+        }
+
+        public static List<List<int>[]> SolveTowerOfHanoiRecursive(int numDiscs, int index, List<List<int>[]> snapshots, List<int> from, List<int> aux, List<int> to)
+        {
+            if (numDiscs <= 1)
+            {
+                to.Add(from[from.Count - 1]);
+                from.Remove(to[to.Count - 1]);
+                return snapshots;
+            }
+
+            snapshots.Add(new List<int>[NUMBER_OF_STICKS]);
+
+            for (int i = 0; i < NUMBER_OF_STICKS; i++)
+            {
+                snapshots[index][i] = new List<int>();
+            }
+
+            return snapshots;
         }
     }
 }
