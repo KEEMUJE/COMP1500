201ec550721c37e8a6e48a85fc30313cb3499008
Kim WooJae <1999.mozzi@gmail.com>
2021-08-03 06:11:06 +0900

    Arena.cs GoToNextTurn Function Build

--- Assignment4/Assignment4/Arena.cs
+++ Assignment4/Assignment4/Arena.cs
@@ -17,32 +17,30 @@ namespace Assignment4
         public uint Turns { get; private set; }
         public uint MonsterCount { get; private set; }
         public Monster ObjectName { get; private set; }
-        public Monster[] Monsters { get; private set; }
+        public List<Monster> Monsters { get; private set; }
 
         public void LoadMonsters(string filepath)
         {
-            const int MONSTER_DATAS = 5;
             try
             {
                 string[] loadMonsterText = File.ReadAllLines(filepath);
-                string[] currentMonsterInfo = new string[MONSTER_DATAS];
-                Monsters = new Monster[loadMonsterText.Length];
+                Monsters = new List<Monster>((int)Capacity);
 
-                for (int i = 0; i < loadMonsterText.Length; ++i)
+                for (int i = 0; i < Capacity; ++i)
                 {
-                    if (MonsterCount == Capacity)
+                    if (i > loadMonsterText.Length - 1)
                     {
                         break;
                     }
 
-                    currentMonsterInfo = loadMonsterText[i].Split(',');
+                    string[] currentMonsterInfo = loadMonsterText[i].Split(',');
                     string name = currentMonsterInfo[0];
                     EElementType elementType = (EElementType)Enum.Parse(typeof(EElementType), currentMonsterInfo[1]);
                     int health = int.Parse(currentMonsterInfo[2]);
                     int attackStat = int.Parse(currentMonsterInfo[3]);
                     int defenseStat = int.Parse(currentMonsterInfo[4]);
 
-                    Monsters[i] = new Monster(name, elementType, health, attackStat, defenseStat);
+                    Monsters.Add(new Monster(name, elementType, health, attackStat, defenseStat));
                     ++MonsterCount;
                 }
             }
@@ -53,6 +51,28 @@ namespace Assignment4
             }
         }
 
+        /*
+        public EElementType GetElementType(string elementType)
+        {
+            switch (elementType)
+            {
+                case "Fire":
+                    return EElementType.Fire;
+                case "Water":
+                    return EElementType.Water;
+                case "Earth":
+                    return EElementType.Earth;
+                case "Wind":
+                    return EElementType.Wind;
+                default:
+                    Debug.Assert(false);
+                    break;
+            }
+
+            return (EElementType)(-1);
+        }
+        */
+
         public void GoToNextTurn()
         {
             if (MonsterCount == 1)
@@ -62,20 +82,19 @@ namespace Assignment4
 
             for (int i = 0; i < MonsterCount; ++i)
             {
-                if (Monsters[i].Health == 0)
-                {
-                    --MonsterCount;
-                    ++i;
-                }
-
                 if (i == MonsterCount - 1)
                 {
                     Monsters[i].Attack(Monsters[0]);
                 }
-
                 else
                 {
                     Monsters[i].Attack(Monsters[i + 1]);
+                }
+
+                if (Monsters[i].Health <= 0)
+                {
+                    Monsters.Remove(Monsters[i]);
+                    --MonsterCount;
                 }
             }
 
@@ -91,7 +110,7 @@ namespace Assignment4
 
             Monster bestMonster = Monsters[0];
 
-            for (int i = 1; i < Monsters.Length; ++i)
+            for (int i = 1; i < MonsterCount; ++i)
             {
                 if (bestMonster.Health < Monsters[i].Health)
                 {
